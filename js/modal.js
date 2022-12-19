4032aa2ee2c697922d3627e6735260f78633e026
mr <ratarby@gmail.com>
2022-12-19 23:57:51 +0100

    fixed some bugs

--- js/modal.js
+++ js/modal.js
@@ -115,115 +115,123 @@ function formVerify() {
     let conditionsChecked;
 
 
-if (!firstNameValue.match(/^\S[a-za-zàáâäçèéêëìíîïñòóôöùúûüA-Z-\s]{2,}$/) // condition si firstName n'est pas conforme avec le regex
-    || firstNameValue == ''   // ou condition si firstNameValue est égale à une chaîne de caractère vide
-    || firstNameValue == null // ou condition si firstNameValue est égale à valeur null
-    || firstNameValue.length < 2) // ou condition si firstNameValue est strictement inférieur à 2 caractères.
-    { // je rentre les arguments suivant
-    errorFirst.innerText = 'Veuillez saisir votre Prénom avec plus de 2 caractères.'; // ajout texte
-    errorFirst.style.color = '#ff0000'; // ajout declaration propriété CSS color red
-    errorFirst.style.fontSize = '.625rem'; // ajout  declaration propriété CSS fontSize de 10px
-    errorFirst.style.marginTop = '8px'; //ajout marginTop de 8 px
-    firstName.style.border = 'solid #ff0000 2px'; // ajout d'une border
-} else { // sinon je rentre les arguments suivant avec une condition vraie
-    errorFirst.style.display = 'none'; // ajout déclaration propriété CSS display none pour cacher l'élémént
-    firstName.style.border = 'none'; // ajout déclaration propriété CSS border none pour enlever la bordure
-    firstNameChecked = true; // valeur booleenne = true
-};
+    if (!firstNameValue.match(/^\S[a-za-zàáâäçèéêëìíîïñòóôöùúûüA-Z-\s]{2,}$/) // condition si firstName n'est pas conforme avec le regex
+        || firstNameValue === ''   // ou condition si firstNameValue est égale à une chaîne de caractère vide
+        || firstNameValue == null // ou condition si firstNameValue est égale à valeur null
+        || firstNameValue.length < 2) // ou condition si firstNameValue est strictement inférieur à 2 caractères.
+        { // je rentre les arguments suivant
+        errorFirst.innerText = 'Veuillez saisir votre Prénom avec plus de 2 caractères'; // ajout texte
+        errorFirst.style.color = '#ff0000'; // ajout declaration propriété CSS color red
+        errorFirst.style.fontSize = '15px'; // ajout  declaration propriété CSS fontSize de 10px
+        errorFirst.style.marginTop = '8px'; //ajout marginTop de 8 px
+        firstName.style.border = 'solid #ff0000 2px'; // ajout d'une border
+        firstName.style.marginBottom = '10px'; // ajout d'une marginBottom
+    } else { // sinon je rentre les arguments suivant avec une condition vraie
+        errorFirst.style.display = 'none'; // ajout déclaration propriété CSS display none pour cacher l'élémént
+        firstName.style.border = 'none'; // ajout déclaration propriété CSS border none pour enlever la bordure
+        firstNameChecked = true; // valeur booleenne = true
+    };
 
-if (!lastNameValue.match(/^\S[a-za-zàáâäçèéêëìíîïñòóôöùúûüA-Z-\s]{2,}$/)
-    || lastNameValue == ''
-    || lastNameValue == null
-    || lastNameValue.length < 2) {
-    errorLast.innerText = 'Veuillez saisir votre Nom avec plus de 2 caractères.';
-    errorLast.style.color = '#ff0000';
-    errorLast.style.fontSize = '.625rem';
-    errorLast.style.marginTop = '8px';
-    lastName.style.border = 'solid red 2px';
-}  else {
-errorLast.style.display = 'none';
-lastName.style.border = 'none';
-lastNameChecked = true;
-};
+    if (!lastNameValue.match(/^\S[a-za-zàáâäçèéêëìíîïñòóôöùúûüA-Z-\s]{2,}$/)
+        || lastNameValue === ''
+        || lastNameValue == null
+        || lastNameValue.length < 2) {
+        errorLast.innerText = 'Veuillez saisir votre Nom avec plus de 2 caractères';
+        errorLast.style.color = '#ff0000';
+        errorLast.style.fontSize = '15px';
+        errorLast.style.marginTop = '8px';
+        lastName.style.border = 'solid red 2px';
+        lastName.style.marginBottom = '10px';
+    }  else {
+    errorLast.style.display = 'none';
+    lastName.style.border = 'none';
+    lastNameChecked = true;
+    };
 
-if (!/^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$/.test(emailValue)) {
-    errorEmail.innerText = 'Veuillez renseigner une addresse mail valide';
-    errorEmail.style.color = '#ff0000';
-    errorEmail.style.fontSize = '0.625rem';
-    errorEmail.style.marginTop = '8px';
-    eMail.style.border = 'solid red 2px';
-} else {
-    errorEmail.style.display = 'none';
-    eMail.style.border = 'none';
-    emailChecked = true;
-};
+    if (!/^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$/.test(emailValue)) {
+        errorEmail.innerText = 'Veuillez renseigner une addresse mail valide';
+        errorEmail.style.color = '#ff0000';
+        errorEmail.style.fontSize = '15px';
+        errorEmail.style.marginTop = '8px';
+        eMail.style.border = 'solid red 2px';
+        eMail.style.marginBottom = '10px';
+    } else {
+        errorEmail.style.display = 'none';
+        eMail.style.border = 'none';
+        emailChecked = true;
+    };
 
-if (!birthDateValue.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
-    errorBirth.innerText = 'Veuillez saisir votre date de Naissance';
-    errorBirth.style.color = '#ff0000';
-    errorBirth.style.fontSize = '.625rem';
-    errorBirth.style.marginTop = '8px';
-    birthDate.style.border = 'solid red 2px';
-} else {
-    errorBirth.style.display = 'none';
-    birthDate.style.border = 'none';
-    birthDateChecked = true;
-};
+    if (!birthDateValue.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
+        errorBirth.innerText = 'Veuillez saisir votre date de Naissance';
+        errorBirth.style.color = '#ff0000';
+        errorBirth.style.fontSize = '15px';
+        errorBirth.style.marginTop = '8px';
+        birthDate.style.border = 'solid red 2px';
+        birthDate.style.marginBottom = '10px';
 
-if (!quantityValue.match(/^[0-9]{1,}$/)) {
-    errorQuantity.innerText = 'Un nombre est requis';
-    errorQuantity.style.color = '#ff0000';
-    errorQuantity.style.fontSize = '.625rem';
-    errorQuantity.style.marginTop = '8px';
-    quantity.style.border = 'solid red 2px';
+    } else {
+        errorBirth.style.display = 'none';
+        birthDate.style.border = 'none';
+        birthDateChecked = true;
+    };
 
-} else {
-    errorQuantity.style.display = 'none';
-    quantity.style.border = 'none';
-    quantityChecked = true;
-};
+    if (!quantityValue.match(/^[0-9]{1,}$/)) {
+        errorQuantity.innerText = 'Un nombre est requis';
+        errorQuantity.style.color = '#ff0000';
+        errorQuantity.style.fontSize = '15px';
+        errorQuantity.style.marginTop = '8px';
+        quantity.style.border = 'solid red 2px';
+        quantity.style.marginBottom = '10px';
+    } else {
+        errorQuantity.style.display = 'none';
+        quantity.style.border = 'none';
+        quantityChecked = true;
+    };
 
-if (!location1.checked
-    && !location2.checked
-    && !location3.checked
-    && !location4.checked
-    && !location5.checked
-    && !location6.checked) {
-    errorCity.innerText = 'Veuillez renseigner une ville';
-    errorCity.style.color = '#ff0000';
-    errorCity.style.fontSize = '.625rem';
-    errorCity.style.marginTop = '8px';
-} else {
-    errorCity.style.display = 'none';
-    radioChecked = true;
-};
+    if (!location1.checked
+        && !location2.checked
+        && !location3.checked
+        && !location4.checked
+        && !location5.checked
+        && !location6.checked) {
+        errorCity.innerText = 'Veuillez renseigner une ville';
+        errorCity.style.color = '#ff0000';
+        errorCity.style.fontSize = '15px';
+        errorCity.style.marginTop = '8px';
+        errorCity.style.marginBottom = '10px';
 
-if (!validCheckbox1.checked) {
-    errorValidation1.innerText = 'Veuillez accepter les termes et les conditions';
-    errorValidation1.style.color = '#ff0000';
-    errorValidation1.style.fontSize = '.625rem';
-    errorValidation1.style.marginTop = '8px';
-} else {
-    errorValidation1.style.display = 'none';
-    conditionsChecked = true;
-};
+    } else {
+        errorCity.style.display = 'none';
+        radioChecked = true;
+    };
+
+    if (!validCheckbox1.checked) {
+        errorValidation1.innerText = 'Veuillez accepter les termes et les conditions';
+        errorValidation1.style.color = '#ff0000';
+        errorValidation1.style.fontSize = '15px';
+        errorValidation1.style.marginTop = '8px';
+        errorValidation1.style.marginBottom = '10px';
+    } else {
+        errorValidation1.style.display = 'none';
+        conditionsChecked = true;
+    };
 
 // #4 confirmation validation avec initialisation des variables en valaur booleenne == "true"
 
-if (firstNameChecked == true            // si toutes les conditions remplissent la valeur booleenne
-    && lastNameChecked == true
-    && emailChecked == true
-    && quantityChecked == true
-    && radioChecked == true
-    && conditionsChecked == true
-    && birthDateChecked == true) { // alors
-// je récupère la constante form et je lui declare  la propriété CSS "style.display" = 'none' pour cacher l' élément form
-    form.style.display = "none";
-// je récupère la constante confirmation et je lui déclare le propriété CSS "style.display" = 'flex' pour afficher le message de remerciement
-    confirmation.style.display = "flex";
+    if (firstNameChecked == true            // si toutes les conditions remplissent la valeur booleenne
+        && lastNameChecked == true
+        && emailChecked == true
+        && quantityChecked == true
+        && radioChecked == true
+        && conditionsChecked == true
+        && birthDateChecked == true) { // alors
+    // je récupère la constante form et je lui declare  la propriété CSS "style.display" = 'none' pour cacher l' élément form
+        form.style.display = "none";
+    // je récupère la constante confirmation et je lui déclare le propriété CSS "style.display" = 'flex' pour afficher le message de remerciement
+        confirmation.style.display = "flex";
 
 
-};
+    };
 };
 
 // #4 Close confirmation
